57983843fcf530bf2544e86521299c532ac23689
Akanksha More <amore4969@gmail.com>
2025-07-28 06:12:55 +0530

    cart routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,15 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ItemDetailsPage from "./pages/ItemDetailsPage";
 import Payment from "./pages/Payment";
+import Cart from "./pages/Cart";
+import WishList from "./pages/WishList";
 
 const router = createBrowserRouter(
     [
       {path: "/", element: <Login />},
       {path: "/register", element: <Register />},
+      {path: "/cart", element: <Cart />},
+      {path: "/wishList", element: <WishList />},
       {path: "/itemDetailsPage", element: <ItemDetailsPage />},
       {path: "/payment", element: <Payment />}
     
