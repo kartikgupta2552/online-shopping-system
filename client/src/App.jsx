6d7bef263f744d449da57011f8cc667dfce4f9c9
Akanksha More <amore4969@gmail.com>
2025-08-03 18:55:39 +0530

    cart , Invoice and order History Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,17 +6,24 @@ import Payment from "./pages/Payment";
 import HomePage from "./pages/HomePage";
 import CategoryPage from "./pages/CategoryPage";
 import SearchResultsPage from "./pages/SearchResultsPage";
+import InvoicePage from "./pages/InvoicePage";
+import OrderHistory from "./pages/OrderHistory";
+import Cart from "./pages/Cart";
 
 const router = createBrowserRouter(
     [
+      
       {path: "/", element: <Login />},
       {path: "/register", element: <Register />},
       {path: "/itemDetailsPage", element: <ItemDetailsPage />},
+      {path: "/cart", element: <Cart />},
       {path: "/payment", element: <Payment />},
       {path: "/homepage", element: <HomePage/>},
       {path: "/category/:name",element:<CategoryPage/>},
-      {path: "/search", element:<SearchResultsPage/>}
-    
+      {path: "/search", element:<SearchResultsPage/>},
+      {path: "/orderHistory", element:<OrderHistory/>},
+      {path:"/invoicePage",element:<InvoicePage/>}
+
     ]
   )
 
