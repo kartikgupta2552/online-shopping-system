c5dadeb19846196ae9f06f3d131d2928d0ae416c
Kartik Gupta <gkartik500@gmail.com>
2025-08-09 23:25:39 +0530

    integrate product detail page api

--- client/src/api/productApi.js
+++ client/src/api/productApi.js
@@ -8,10 +8,20 @@ const productApi = {
     return response.data;
   },
 
+  getProductBySubCategoryId: async(subCategoryId) => {
+    const response = await axios.get(`${BASE_URL}/product/subcategory/${subCategoryId}`)
+    return response.data
+  },
+
   searchProduct: async (keyword) => {
     const response = await axios.get(`${BASE_URL}/product/search`, {
         params : { keyword : keyword }
     })
+    return response.data
+  },
+
+  getProductById: async (productId) => {
+    const response = await axios.get(`${BASE_URL}/product/${productId}`)
     return response.data
   }
 };
